f7e871fd7a69f81c3ecb054e8d638a656f024b88
Hasan Şimşek <168709291+hasnnsmskk@users.noreply.github.com>
2024-09-08 16:54:41 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import "./App.css"
 import { BrowserRouter as Router, Route, Routes, createBrowserRouter } from 'react-router-dom'
-import HomePage from '../src/pages/HomePage'
+import HomePage from './pages/HomePage'
 import ShopPage from './pages/ShopPage'
 function App() {
   return (

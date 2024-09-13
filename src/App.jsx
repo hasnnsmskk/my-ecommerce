b6a3f5868741b827405fda963afae3efb9b24cf4
Hasan Şimşek <168709291+hasnnsmskk@users.noreply.github.com>
2024-09-13 18:32:36 +0300

    Shop page yapıldı.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import "./App.css"
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from "./HomePage"
+import ShopPage from "./ShopPage"
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </Router>
   )

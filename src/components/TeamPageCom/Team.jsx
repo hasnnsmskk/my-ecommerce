6d2dff348f8161a1327674fe1521f0d92cae0d6b
Hasan Şimşek <168709291+hasnnsmskk@users.noreply.github.com>
2024-09-25 17:49:01 +0300

    Tasarım güncellendi.

--- src/components/TeamPageCom/Team.jsx
+++ src/components/TeamPageCom/Team.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react';
 import { FaFacebook, FaInstagram, FaTwitter, FaLinkedin } from 'react-icons/fa';
-
-
+import ali from "../../assets/TeamPageİmg/ali.jpg"
+import ayse from "../../assets/TeamPageİmg/ayse.jpg"
 import erhan from "../../assets/TeamPageİmg/erhanfırat.png"
 import gokhan from "../../assets/TeamPageİmg/gokhanozdemir.png"
 import hasan from "../../assets/TeamPageİmg/hasan.png"
+import mehmet from "../../assets/TeamPageİmg/mehmet.jpg"
+import muge from "../../assets/TeamPageİmg/muge.jpg"
+import selin from "../../assets/TeamPageİmg/selin.jpg"
+import deniz from "../../assets/TeamPageİmg/deniz.jpg"
 const Teampage = () => {
   const teamMembers = [
     { name: 'Erhan FIRAT', role: 'Project Owner', image: erhan },
     { name: 'Gökhan Özdemir', role: 'Scrum Master', image: gokhan },
     { name: 'Hasan Şimşek', role: 'Full Stack Developer', image: hasan },
-    { name: 'Ali Veli', role: 'Frontend Developer', image: 'path_to_member1_image.jpg' },
-    { name: 'Ayşe Kaya', role: 'Backend Developer', image: 'path_to_member2_image.jpg' },
-    { name: 'Mehmet Can', role: 'UI/UX Designer', image: 'path_to_member3_image.jpg' },
-    // Diğer takım üyeleri
+    { name: 'Ali Veli', role: 'Frontend Developer', image: ali },
+    { name: 'Ayşe Kaya', role: 'Backend Developer', image: ayse },
+    { name: 'Mehmet Can', role: 'UI/UX Designer', image: mehmet },
+    { name: 'Selin Yıldırım', role: 'Backend Developer', image: selin },
+    { name: 'Müge Paksoy', role: 'Ftontend Developer', image: muge },
+    { name: 'Deniz Kök', role: 'UI/UX Designer', image: deniz },
+
   ];
 
   return (
@@ -24,11 +31,11 @@ const Teampage = () => {
 
       {/* Team Section */}
       <section className="bg-white py-16 md:ml-56 md:mr-56">
-        <h2 className="text-3xl font-bold text-center mb-12">Meet Our Team</h2>
-        <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8">
+        <h2 className="text-3xl font-bold text-center mb-4">Meet Our Team</h2>
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-16">
           {/* Dynamic Team Members */}
           {teamMembers.map((member, index) => (
-            <div key={index} className="text-center">
+            <div key={index} className="text-center md:mb-16">
               <img src={member.image} alt={member.name} className="w-full h-80 object-cover  mb-4" />
               <h3 className="text-lg font-bold">{member.name}</h3>
               <p className="text-gray-500">{member.role}</p>
